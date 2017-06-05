a7a292a61e768e2181aaa07024209d6f3c7b2cbc
pjsier <pjsier@gmail.com>
2017-06-05 09:14:56 -0400

    misc updates on styling, layout

--- 404.php
+++ 404.php
@@ -9,56 +9,63 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-7 col-md-offset-1 content-col">
+					<section class="error-404 not-found">
+						<header class="page-header">
+							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mto_custom' ); ?></h1>
+						</header><!-- .page-header -->
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'mto_custom' ); ?></h1>
-				</header><!-- .page-header -->
+						<div class="page-content">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mto_custom' ); ?></p>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'mto_custom' ); ?></p>
+							<?php
+								get_search_form();
 
-					<?php
-						get_search_form();
+								the_widget( 'WP_Widget_Recent_Posts' );
 
-						the_widget( 'WP_Widget_Recent_Posts' );
+								// Only show the widget if site has multiple categories.
+								if ( mto_custom_categorized_blog() ) :
+							?>
 
-						// Only show the widget if site has multiple categories.
-						if ( mto_custom_categorized_blog() ) :
-					?>
+							<div class="widget widget_categories">
+								<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'mto_custom' ); ?></h2>
+								<ul>
+								<?php
+									wp_list_categories( array(
+										'orderby'    => 'count',
+										'order'      => 'DESC',
+										'show_count' => 1,
+										'title_li'   => '',
+										'number'     => 10,
+									) );
+								?>
+								</ul>
+							</div><!-- .widget -->
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'mto_custom' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
+							<?php
+								endif;
 
-					<?php
-						endif;
+								/* translators: %1$s: smiley */
+								$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'mto_custom' ), convert_smilies( ':)' ) ) . '</p>';
+								the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'mto_custom' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+								the_widget( 'WP_Widget_Tag_Cloud' );
+							?>
 
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+						</div><!-- .page-content -->
+					</section><!-- .error-404 -->
+				</div>
+				<div class="col-md-2 aside-col">
+					<?php get_sidebar(); ?>
+				</div>
+			</div>
+		</div>
+	</main><!-- #main -->
+</div><!-- #primary -->
 
 <?php
 get_footer();
