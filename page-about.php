3081788308c80d2b72a91d9c1f5d619f7e0af058
pjsier <pjsier@gmail.com>
2017-09-17 09:18:53 -0500

    Further updates to about

--- page-about.php
+++ page-about.php
@@ -14,7 +14,7 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area container-fluid">
+<div id="primary" class="content-area about container-fluid">
 	<main id="main" class="site-main about" role="main">
 		<div class="row about-hero">
 			<div class="col-md-7 col-md-offset-1 text">
@@ -33,7 +33,7 @@ get_header(); ?>
 						mission of MTO. 
 					</p>
 					<div class="cta-arrow-link">
-						<a href="#">
+						<a href="#history-carousel-container" data-toggle="collapse">
 						<svg class="arrow-icon medium down"
 							sversion="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
 							viewBox="0 0 110 110" xml:space="preserve">
@@ -43,8 +43,8 @@ get_header(); ?>
 					</div>
 				</div>
 			</div>
-			<div class="row">
-				<div id="history-carousel" class="carousel slide" data-ride="carousel">
+			<div id="history-carousel-container" class="row collapse">
+				<div id="history-carousel" class="carousel slide" data-ride="carousel" data-interval="false">
 					<ol class="carousel-indicators">
 						<li data-target="#history-carousel" data-slide-to="0" class="active"></li>
 						<li data-target="#history-carousel" data-slide-to="1"></li>
@@ -53,42 +53,101 @@ get_header(); ?>
 					</ol>
 					<div class="carousel-inner">
 						<div class="item active">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-01.png" alt="MTO organizers">
+							<img class="carousel" src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-01.png" alt="MTO organizers">
 							<div class="carousel-caption">
 								<h3>Chicago's need for an organization to represent tenants</h3>
 								<p>The Metropolitan Tenants Organization (MTO) is rooted in a 1981 Community Congress convened to find workable solutions to the affordable housing crisis at the time. The Congress recognized that Chicago needed a citywide organization to represent tenants.</p>
 							</div>
+							<a class="left carousel-control" href="#history-carousel" role="button" data-slide="prev">
+								<img class="chevron chevron-left" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#history-carousel" role="button" data-slide="next">
+								<img class="chevron chevron-right" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Next</span>
+							</a>
 						</div>
 						<div class="item">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-02.png" alt="Carousel 2">
+							<img class="carousel" src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-02.png" alt="Carousel 2">
 							<div class="carousel-caption">
 								<h3>Establishment of Residential Landlord and Tenant Ordinance</h3>
 								<p>It led to a 5-year campaign to pass Chicago's landmark Residential Landlord and Tenant Ordinance (RLTO) and the founding of MTO. Passage of the RLTO is a watershed event in MTO's history.</p>
 							</div>
+							<a class="left carousel-control" href="#history-carousel" role="button" data-slide="prev">
+								<img class="chevron chevron-left" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#history-carousel" role="button" data-slide="next">
+								<img class="chevron chevron-right" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Next</span>
+							</a>
 						</div>
 						<div class="item">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-03.png" alt="Carousel 3">
+							<img class="carousel" src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-03.png" alt="Carousel 3">
 							<div class="carousel-caption">
 								<h3>MTO's commitment to empower tenants</h3>
 								<p>Since the passage of the RLTO, MTO has remained committed to the mission of educating, organizing and empowering tenants to have a voice in the decisions that effect the availability and affordability of safe and decent housing.</p>
 							</div>
+							<a class="left carousel-control" href="#history-carousel" role="button" data-slide="prev">
+								<img class="chevron chevron-left" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#history-carousel" role="button" data-slide="next">
+								<img class="chevron chevron-right" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Next</span>
+							</a>
 						</div>
 						<div class="item">
-							<img src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-04.png" alt="Carousel 4">
+							<img class="carousel" src="<?php echo get_template_directory_uri(); ?>/images/mto-hist-04.png" alt="Carousel 4">
 							<div class="carousel-caption">
 								<h3>Our primary goals for tenants</h3>
 								<p>Our primary goals are to better tenants' living conditions and preserve housing, stabilize tenancies and prevent homelessness and improve communication between landlords and tenants.</p>
 							</div>
+							<a class="left carousel-control" href="#history-carousel" role="button" data-slide="prev">
+								<img class="chevron chevron-left" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#history-carousel" role="button" data-slide="next">
+								<img class="chevron chevron-right" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
+								<span class="sr-only">Next</span>
+							</a>
 						</div>
 					</div>
-					<a class="left carousel-control" href="#history-carousel" role="button" data-slide="prev">
-						<img class="chevron chevron-left" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
-						<span class="sr-only">Previous</span>
-					</a>
-					<a class="right carousel-control" href="#history-carousel" role="button" data-slide="next">
-						<img class="chevron chevron-right" aria-hidden="true" src="<?php echo get_template_directory_uri(); ?>/images/chevron.svg"></span>
-						<span class="sr-only">Next</span>
-					</a>
+				</div>
+			</div>
+		</div>
+		<div class="our-principles no-row-margins">
+			<div class="row">
+				<div class="col-md-5 left no-row-margins">
+					<div class="row">
+						<div class="col-md-offset-2">
+							<h2>Our Principles</h2>
+							<p>What are principles?  Why are they important? Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut.</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-7 right no-row-margins tab-content">
+					<ul class="nav st-nav-tabs">
+						<li class="active">
+							<a href="#principles-tab1" data-toggle="tab">Affordability</a>
+						</li>
+						<li>
+							<a href="#principles-tab2" data-toggle="tab">Development</a>
+						</li>
+						<li>
+							<a href="#principles-tab3" data-toggle="tab">Organizing</a>
+						</li>
+					</ul>
+					<div class="row tab-pane active" id="principles-tab1">
+						<div>Affordability</div>
+					</div>
+					<div class="row tab-pane" id="principles-tab2">
+						<div>Development</div>
+					</div>
+					<div class="row tab-pane" id="principles-tab3">
+						<div>Organizing</div>
+					</div>
+					</div>
 				</div>
 			</div>
 		</div>
